df08574800759c0ddf31289e3ede7440945bca0d
angeldiaz17 <74811335+angeldiaz17@users.noreply.github.com>
2026-03-17 21:39:48 -0600

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-package GAMEUNO;
-public class Main{
-    public static void main(String[] args) {
-    // Se crea un nuevo juego
-        Game juego = new Game();
-    // Se inicia el juego
-        juego.iniciar();
-    }
-}
